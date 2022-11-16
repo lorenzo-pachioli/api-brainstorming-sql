8e5d9f8e90a5e4e85c128bfcb5e4d53de39dc491
lpachioli <lorenzopachioli@gmail.com>
2022-11-16 19:35:40 -0300

    Login service finished

--- src/app/services/LoginService.js
+++ src/app/services/LoginService.js
@@ -1,16 +1,35 @@
 const jwt = require('jsonwebtoken');
+const UsersBrainstorming = require('../models/UsersModel');
 
-exports.LoginService = (user, res) => {
-  let jwtSecretKey = process.env.JWT_SECRET_KEY;
-  let data = {
-    time: Date(),
-    userId: 12,
-  };
+exports.LoginService = async (user, res) => {
 
-  const token = jwt.sign(data, jwtSecretKey);
+  try {
+    const userExist = await UsersBrainstorming.findOne({ username: user.username });
 
-  res.status(200).json({
-    msj: `correct login user ${user.username}`,
-    token: token
-  });
+    if (userExist) {
+      let jwtSecretKey = process.env.JWT_SECRET_KEY;
+      let data = {
+        time: Date(),
+        userId: userExist.id,
+      };
+
+      const token = jwt.sign(data, jwtSecretKey);
+      return res.status(200).json({
+        msj: `correct login user ${user.username}`,
+        token: token
+      });
+    }
+
+    return res.status(200).json({
+      msj: `User ${user.username} doesn't exist`,
+      token: ""
+    });
+
+  } catch (err) {
+    console.log('error', err);
+    res.status(503).json({
+      msj: `Couldn't login`,
+      content: []
+    });
+  }
 }
